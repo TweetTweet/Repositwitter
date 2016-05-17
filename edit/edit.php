011a25e1ad0711ece9f638b9200819727dffdea2
Ray <ruemu.digba@ucc.on.ca>
2016-05-17 17:16:32 -0400

    not much going on here either

--- edit/edit.php
+++ edit/edit.php
@@ -157,7 +157,7 @@ if (substr($name, 0, 1) === "a")
  </div>
     <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
-        <script src="js/index.js"></script>
+        <script src="index.js"></script>
    
 </div>
 	</body>
